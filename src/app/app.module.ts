20a9f4ed7ca0b91904ff25a5acec8a081a46ff48
Cap'n_Rob <robb1772@hotmail.com>
2018-07-07 13:30:18 -0700

    added navbar with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
